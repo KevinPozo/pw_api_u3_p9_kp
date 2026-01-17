f7d89e578e0443b5b3676e906ca1d297b38e8d2e
Kevin Fernando Pozo Maldonado <kevinferpozo@gmail.com>
2026-01-16 19:40:15 -0500

    Taller 26

--- src/main/java/uce/edu/web/api/matricula/application/EstudianteService.java
+++ src/main/java/uce/edu/web/api/matricula/application/EstudianteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import uce.edu.web.api.matricula.domain.Estudiante;
 import uce.edu.web.api.matricula.infraestructure.EstudianteRepository;
 
@@ -14,5 +15,44 @@ public class EstudianteService {
 
     public List<Estudiante> listarTodos() {
         return this.estudianteRepository.listAll();
+    }
+
+    public Estudiante consultarPorId(Integer id) {
+        return this.estudianteRepository.findById(id.longValue());
+    }
+
+    @Transactional
+    public void crear(Estudiante estu) {
+        this.estudianteRepository.persist(estu);
+    }
+
+    @Transactional
+    public void actualizar(Integer id, Estudiante est) {
+        Estudiante estu = this.consultarPorId(id);
+        estu.setApellido(est.getApellido());
+        estu.setNombre(est.getNombre());
+        estu.setFechaNacimiento(est.getFechaNacimiento());
+        // Se actualiza automaticametne por dirty checking
+    }
+
+    @Transactional
+    public void actualizarParcial(Integer id, Estudiante est) {
+        Estudiante estu = this.consultarPorId(id);
+        if (est.getNombre() != null) {
+            estu.setNombre(est.getNombre());
+        }
+        if (est.getApellido() != null) {
+            estu.setApellido(est.getApellido());
+        }
+        if (est.getFechaNacimiento() != null) {
+            estu.setFechaNacimiento(est.getFechaNacimiento());
+        }
+        // Se actualiza automaticametne por dirty checking
+
+    }
+
+    @Transactional
+    public void eliminar(Integer id) {
+        this.estudianteRepository.deleteById(id.longValue());
     }
 }
