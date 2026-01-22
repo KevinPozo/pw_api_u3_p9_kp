3846575d9ee30b7ab537e0c3160422bb0d8aa9af
Kevin Fernando Pozo Maldonado <kevinferpozo@gmail.com>
2026-01-21 19:48:27 -0500

    Taller 28

--- src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
@@ -20,39 +20,41 @@ public class EstudianteResource {
     private EstudianteService estudianteService;
 
     @GET
-    @Path("/todos")
+    @Path("")
     public List<Estudiante> listarTodos() {
+        System.out.println("ALISTAR TODOS XXXXXXXXXXXXX");
         return this.estudianteService.listarTodos();
     }
     @GET
-    @Path("/consultarPorId/{id}")
+    @Path("/{id}")
     public Estudiante consultarPorId(@PathParam("id") Integer id) {
         return this.estudianteService.consultarPorId(id);
     }
 
     @POST
-    @Path("/guardar")
+    @Path("")
     public void guardarEstudiante(Estudiante estu){
         this.estudianteService.crear(estu);
     }
     @PUT
-    @Path("/actualizar/{id}")
+    @Path("/{id}")
     public void actualizarEstudiante(@PathParam("id") Integer id, Estudiante estu){
         this.estudianteService.actualizar(id, estu);
     }
     @PATCH
-    @Path("/actualizarParcial/{id}")
+    @Path("/{id}")
     public void actualizarEstudianteParcial(@PathParam("id") Integer id, Estudiante estu){
         this.estudianteService.actualizarParcial(id, estu);
     }
     @DELETE
-    @Path("/eliminar/{id}")
+    @Path("/{id}")
     public void eliminarEstudiante(@PathParam("id") Integer id){
         this.estudianteService.eliminar(id);
     }
     @GET
-    @Path("/buscarPorProvincia")
+    @Path("/provincia/genero")
     public List<Estudiante> buscarPorProvincia(@QueryParam("provincia") String provincia, @QueryParam("genero") String genero) {
+        System.out.println("ALISTAR POR PROVINCIA Y GENERO XXXXXXXXXXXXX");
         return this.estudianteService.buscarPorProvincia(provincia, genero);
     }
 }
--- src/main/java/uce/edu/web/api/matricula/interfaces/MateriaResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/MateriaResource.java
@@ -10,6 +10,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 import uce.edu.web.api.matricula.application.MateriaService;
 import uce.edu.web.api.matricula.domain.Materia;
 
@@ -20,49 +21,45 @@ public class MateriaResource {
     private MateriaService materiaService;
 
     @GET
-    @Path("/todos")
-    public List<Materia> listarTodos() {
+    @Path("")
+    public List<Materia> listarTodos(@QueryParam("codigo") String codigo, @QueryParam("semestre") String semestre) {
+        if (codigo != null && !codigo.isEmpty()) {
+            Materia m = this.materiaService.buscarPorCodigo(codigo);
+            return m != null ? List.of(m) : List.of();
+        }
+        if (semestre != null && !semestre.isEmpty()) {
+            Materia m = this.materiaService.buscarPorSemestre(semestre);
+            return m != null ? List.of(m) : List.of();
+        }
         return this.materiaService.listarTodos();
     }
 
     @GET
-    @Path("/consultarPorId/{id}")
+    @Path("/{id}")
     public Materia consultarPorId(@PathParam("id") Integer id) {
         return this.materiaService.consultarPorId(id);
     }
 
-    @GET
-    @Path("/buscar/codigo/{codigo}")
-    public Materia buscarPorCodigo(@PathParam("codigo") String codigo) {
-        return this.materiaService.buscarPorCodigo(codigo);
-    }
-
-    @GET
-    @Path("/buscar/semestre/{semestre}")
-    public Materia buscarPorSemestre(@PathParam("semestre") String semestre) {
-        return this.materiaService.buscarPorSemestre(semestre);
-    }
-
     @POST
-    @Path("/guardar")
+    @Path("")
     public void guardarMateria(Materia materia) {
         this.materiaService.crear(materia);
     }
 
     @PUT
-    @Path("/actualizar/{id}")
+    @Path("/{id}")
     public void actualizarMateria(@PathParam("id") Integer id, Materia materia) {
         this.materiaService.actualizar(id, materia);
     }
 
     @PATCH
-    @Path("/actualizarParcial/{id}")
+    @Path("/{id}")
     public void actualizarMateriaParcial(@PathParam("id") Integer id, Materia materia) {
         this.materiaService.actualizarParcial(id, materia);
     }
 
     @DELETE
-    @Path("/eliminar/{id}")
+    @Path("/{id}")
     public void eliminarMateria(@PathParam("id") Integer id) {
         this.materiaService.eliminar(id);
     }
